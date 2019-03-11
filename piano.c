5f4e259a9a2585c521f4492f04e23826e5e83af0
ProfZijlstra <mzijlstra@mum.edu>
2019-03-11 16:59:15 +0100

    draw my own piano keys, and redid key to tone mapping

--- piano.c
+++ piano.c
@@ -14,99 +14,243 @@ typedef enum WaveForm {
 } WaveForm;
 
 typedef struct Key {
-    bool on;
-    double freq;
-    double wave_part;
+    char tone[4];   // name of tone, eg A4
+    double freq;    // frequency of tone, eg 440hz
+    char key;       // keyboard scan code
+    SDL_Rect rect;  // rectangle on screen
+    bool on;        // currently playing
+    double wave_part;// place to store wave position between frames
 } Key;
 
+typedef struct Keys {
+    Key *white;     // pointer to array of 'white' keys
+    int w_len;      // how many white keys there are
+    Key *black;     // pointer to array of 'black' keys
+    int b_len;      // how many black keys there are
+} Keys;
+
 static SDL_AudioSpec have;
-static WaveForm wave = sine;
+
+// TODO create a .ini config file (can read using lib iniParser) 
+static WaveForm wave = square;
 static Sint8 volume = 10;
+static double A4 = 432;
 
-void setupKeys(Key* keys) {
-    // make sure the values in keys start at zero;
-    SDL_memset(keys, 0, sizeof(Key) * 256);
+/* initializes arrays of black and white struct Key, starting from the key
+ * indicated by s_key in s_octave, ending on e_key in e_octave
+ *   alwas start and end with a white key
+ */
 
-    keys['1'].freq =            64.22;
-    keys['1' + 128].freq =      68.04;
-    keys['2'].freq =            72.08;
-    keys['2' + 128].freq =      76.37;
-    keys['3'].freq =            80.91;
-    keys['4'].freq =            85.72;
-    keys['4' + 128].freq =      90.82;
-    keys['5'].freq =            96.22;
-    keys['5' + 128].freq =     101.94;
-    keys['6'].freq =           108.00;
-    keys['6' + 128].freq =     114.42;
-    keys['7'].freq =           121.23;
-    keys['8'].freq =           128.43;
-    keys['8' + 128].freq =     136.07;
-    keys['9'].freq =           144.16;
-    keys['9' + 128].freq =     152.74;
-    keys['0'].freq =           161.82;
-    keys['q'].freq =           171.44;
-    keys['q' + 128].freq =     181.63;
-    keys['w'].freq =           192.43;
-    keys['w' + 128].freq =     203.88;
-    keys['e'].freq =           216.00;
-    keys['e' + 128].freq =     228.84;
-    keys['r'].freq =           242.45;
-    keys['t'].freq =           256.87;
-    keys['t' + 128].freq =     272.14;
-    keys['y'].freq =           288.33;
-    keys['y' + 128].freq =     305.47;
-    keys['u'].freq =           323.64;
-    keys['i'].freq =           342.88;
-    keys['i' + 128].freq =     363.27;
-    keys['o'].freq =           384.87;
-    keys['o' + 128].freq =     407.75;
-    keys['p'].freq =           432.00;
-    keys['p' + 128].freq =     457.69;
-    keys['a'].freq =           484.90;
-    keys['s'].freq =           513.74;
-    keys['s' + 128].freq =     544.29;
-    keys['d'].freq =           576.65;
-    keys['d' + 128].freq =     610.94;
-    keys['f'].freq =           647.27;
-    keys['g'].freq =           685.76;
-    keys['g'+ 128].freq =      726.53;
-    keys['h'].freq =           769.74;
-    keys['h'+ 128].freq =      815.51;
-    keys['j'].freq =           864.00;
-    keys['j'+ 128].freq =      915.38;
-    keys['k'].freq =           969.81;
-    keys['l'].freq =          1027.47;
-    keys['l'+ 128].freq =     1088.57;
-    keys['z'].freq =          1153.30;
-    keys['z'+ 128].freq =     1221.88;
-    keys['x'].freq =          1294.54;
-    keys['c'].freq =          1371.51;
-    keys['c' + 128].freq =    1453.07;
-    keys['v'].freq =          1539.47;
-    keys['v' + 128].freq =    1631.01;
-    keys['b'].freq =          1728.00;
-    keys['b' + 128].freq =    1830.75;
-    keys['n'].freq =          1939.61;
-    keys['m'].freq =          2054.95;
-
+bool keyToTone(Key* keys, int len, char *s, char c) {
+    for (int i = 0; i < len; i++) {
+        if (strncmp(keys[i].tone, s, 3) == 0) {
+            keys[i].key = c;
+            return true;
+        }
+    }
+    return false;
 }
 
-void AudioCallback(void *userdata, Uint8 *stream, int len){
+void setupKeys(Keys* keys,
+        char s_key, int s_octave, char e_key, int e_octave) {
+    extern double A4;
+
+    Key* white = keys->white;
+    Key* black = keys->black;
+
+    // start by makeing sure the everything is zero
+    SDL_memset(white, 0, sizeof(Key) * keys->w_len);
+    SDL_memset(black, 0, sizeof(Key) * keys->b_len);
+
+    // generate tonal frequencies based on our A4 val
+    double tone = A4;
+    double tones[12];
+    double next_octave = A4 * 2;
+    for (int i = 0; i < 12; i++) {
+        tones[i] = tone;
+        tone = tone / 2 * 3;
+        if (tone > next_octave) {
+            tone = tone / 2;
+        }
+    }
+    // sort them ascending
+    double sorted[12];
+    for (int i = 0; i < 12; i++) {
+        double smallest = 100000;
+        int small_pos = -1;
+        for (int j = 0; j < 12; j++) {
+            if (tones[j] < smallest) {
+                smallest = tones[j];
+                small_pos = j;
+            }
+        }
+        sorted[i] = tones[small_pos];
+        tones[small_pos] = 100000;
+    }
+    // go to requested start octave (cannot be > 4)
+    int octave = 4;
+    while (octave > s_octave) {
+        for (int i = 0; i < 12; i++) {
+            sorted[i] /= 2;
+        }
+        octave--;
+    }
+    // go to requested start key
+    char bw[] = {'w','b','w','w','b','w','b','w','w','b','w','b'};
+    int index = 0;
+    char k = 'A';
+    while (k < s_key) {
+        index++;
+        if (bw[index] == 'b') {
+            index++;
+        }
+        k++;
+    }
+    // setup keys of the first (requested) octave
+    int wi = 0;
+    int bi = 0;
+    while (index < 12) {
+        if (bw[index] == 'w') {
+            white[wi].tone[0] = k;
+            white[wi].tone[1] = 0x30 + octave;
+            white[wi].freq = sorted[index];
+            wi++;
+        } else { // 'b'
+            black[bi].tone[0] = k;
+            black[bi].tone[1] = 0x30 + octave;
+            black[bi].tone[2] = '#';
+            black[bi].freq = sorted[index];
+            bi++;
+        }
+        if (index < 11 && bw[index + 1] == 'w') {
+            k++;
+        }
+        index++;
+    }
+    octave++;
+    for (int i = 0; i < 12; i++) {
+        sorted[i] *= 2;
+    }
+    // do all the in-between octaves
+    while (octave < e_octave) {
+        k = 'A';
+        for (index = 0; index < 12; index++) {
+            if (bw[index] == 'w') {
+                white[wi].tone[0] = k;
+                white[wi].tone[1] = 0x30 + octave;
+                white[wi].freq = sorted[index];
+                wi++;
+            } else { // 'b'
+                black[bi].tone[0] = k;
+                black[bi].tone[1] = 0x30 + octave;
+                black[bi].tone[2] = '#';
+                black[bi].freq = sorted[index];
+                bi++;
+            }
+            if (index < 11 && bw[index + 1] == 'w') {
+                k++;
+            }
+        }
+        octave++;
+        for (int i = 0; i < 12; i++) {
+            sorted[i] *= 2;
+        }
+    }
+    // then the last octave
+    index = 0;
+    k = 'A';
+    while (k <= e_key && wi < keys->w_len) {
+        if (bw[index] == 'w') {
+            white[wi].tone[0] = k;
+            white[wi].tone[1] = 0x30 + octave;
+            white[wi].freq = sorted[index];
+            wi++;
+        } else { // 'b'
+            black[bi].tone[0] = k;
+            black[bi].tone[1] = 0x30 + octave;
+            black[bi].tone[2] = '#';
+            black[bi].freq = sorted[index];
+            bi++;
+        }
+        if (index < 11 && bw[index + 1] == 'w') {
+            k++;
+        }
+        index++;
+    }
+
+    // TODO these bindings should probably be read from a config file
+    keyToTone(white, 36, "C2", '1');
+    keyToTone(white, 36, "D2", '2');
+    keyToTone(white, 36, "E2", '3');
+    keyToTone(white, 36, "F2", '4');
+    keyToTone(white, 36, "G2", '5');
+    keyToTone(white, 36, "A3", '6');
+    keyToTone(white, 36, "B3", '7');
+    keyToTone(white, 36, "C3", '8');
+    keyToTone(white, 36, "D3", '9');
+    keyToTone(white, 36, "E3", '0');
+    keyToTone(white, 36, "F3", 'q');
+    keyToTone(white, 36, "G3", 'w');
+    keyToTone(white, 36, "A4", 'e');
+    keyToTone(white, 36, "B4", 'r');
+    keyToTone(white, 36, "C4", 't');
+    keyToTone(white, 36, "D4", 'y');
+    keyToTone(white, 36, "E4", 'u');
+    keyToTone(white, 36, "F4", 'i');
+    keyToTone(white, 36, "G4", 'o');
+    keyToTone(white, 36, "A5", 'p');
+    keyToTone(white, 36, "B5", 'a');
+    keyToTone(white, 36, "C5", 's');
+    keyToTone(white, 36, "D5", 'd');
+    keyToTone(white, 36, "E5", 'f');
+    keyToTone(white, 36, "F5", 'g');
+    keyToTone(white, 36, "G5", 'h');
+    keyToTone(white, 36, "A6", 'j');
+    keyToTone(white, 36, "B6", 'k');
+    keyToTone(white, 36, "C6", 'l');
+    keyToTone(white, 36, "D6", 'z');
+    keyToTone(white, 36, "E6", 'x');
+    keyToTone(white, 36, "F6", 'c');
+    keyToTone(white, 36, "G6", 'v');
+    keyToTone(white, 36, "A7", 'b');
+    keyToTone(white, 36, "B7", 'n');
+    keyToTone(white, 36, "C7", 'm');
+
+    keyToTone(black, 25, "C2#", '1');
+    keyToTone(black, 25, "D2#", '2');
+    keyToTone(black, 25, "F2#", '4');
+    keyToTone(black, 25, "G2#", '5');
+    keyToTone(black, 25, "A3#", '6');
+    keyToTone(black, 25, "C3#", '8');
+    keyToTone(black, 25, "D3#", '9');
+    keyToTone(black, 25, "F3#", 'q');
+    keyToTone(black, 25, "G3#", 'w');
+    keyToTone(black, 25, "A4#", 'e');
+    keyToTone(black, 25, "C4#", 't');
+    keyToTone(black, 25, "D4#", 'y');
+    keyToTone(black, 25, "F4#", 'i');
+    keyToTone(black, 25, "G4#", 'o');
+    keyToTone(black, 25, "A5#", 'p');
+    keyToTone(black, 25, "C5#", 's');
+    keyToTone(black, 25, "D5#", 'd');
+    keyToTone(black, 25, "F5#", 'g');
+    keyToTone(black, 25, "G5#", 'h');
+    keyToTone(black, 25, "A6#", 'j');
+    keyToTone(black, 25, "C6#", 'l');
+    keyToTone(black, 25, "D6#", 'z');
+    keyToTone(black, 25, "F6#", 'c');
+    keyToTone(black, 25, "G6#", 'v');
+    keyToTone(black, 25, "A7#", 'b');
+}
+
+int addFrequencies(Sint32 *audio, int alen, Key* keys, int klen) {
     extern WaveForm wave;
     extern SDL_AudioSpec have; // only works if we actually have Sint8
     extern Sint8 volume;
-    Key *keys = (Key*)userdata;
-
-    // first ensure silence in the stream
-    SDL_memset(stream, have.silence, len);
-
-    // create an audio stream to contain all pressed keys in one
-    Sint32 *audio = SDL_malloc(sizeof(Sint32) * len);
-    SDL_memset(audio, 0, sizeof(Sint32) * len);
 
     // find which keys are pressed and mix in their frequencies
     int pressed = 0;
-    for (int k = 0; k < 255; k++) {
+    for (int k = 0; k < klen; k++) {
         if (keys[k].on && keys[k].freq) {
             pressed += 1;
 
@@ -114,7 +258,7 @@ void AudioCallback(void *userdata, Uint8 *stream, int len){
             double part = 0;
             if (wave == square) {
                 double half = tone / 2;
-                for ( int i = 0; i < len; i++ ) {
+                for ( int i = 0; i < alen; i++ ) {
                     part = fmod(keys[k].wave_part + i, tone);
                     if ( part < half ) {
                         audio[i] -= volume;
@@ -126,7 +270,7 @@ void AudioCallback(void *userdata, Uint8 *stream, int len){
                 double qtone = tone * 0.35;
                 double htone = tone * 0.5;
                 double ttone = tone * 0.75;
-                for (int i = 0; i < len; i++) {
+                for (int i = 0; i < alen; i++) {
                     part = fmod(keys[k].wave_part + i, tone);
                     if (part <= qtone) {
                         audio[i] = (part / qtone) * volume;
@@ -139,24 +283,24 @@ void AudioCallback(void *userdata, Uint8 *stream, int len){
                     }
                 }
             } else if (wave == saw) {
-                for (int i = 0; i < len; i++) {
+                for (int i = 0; i < alen; i++) {
                     part = fmod(keys[k].wave_part + i, tone);
-                    audio[i] = -1 + part/tone*2 * volume;
+                    audio[i] = (-1.0 + part/tone*2) * volume;
                 }
             } else if (wave == noise) {
-                for (int i = 0; i < len; i++) {
+                for (int i = 0; i < alen; i++) {
                     audio[i] = ((random() % 20) - 10) * volume; 
                 }
             } else if (wave == sine) {
                 double sine_tone = tone / (2 * M_PI);
-                for (int i = 0; i < len; i++) {
+                for (int i = 0; i < alen; i++) {
                     part = fmod(keys[k].wave_part + i, tone);
                     audio[i] = sin(part / sine_tone) * volume;
                 }
             } else if (wave == opl2_1) {
                 double sine_tone = tone / (2 * M_PI);
                 double htone = tone * 0.5;
-                for (int i = 0; i < len; i++) {
+                for (int i = 0; i < alen; i++) {
                     part = fmod(keys[k].wave_part + i, tone);
                     if (part <= htone) {
                         audio[i] = sin(part / sine_tone) * volume;
@@ -167,7 +311,7 @@ void AudioCallback(void *userdata, Uint8 *stream, int len){
             } else if (wave == opl2_2) {
                 double sine_tone = tone / (2 * M_PI);
                 double htone = tone * 0.5;
-                for (int i = 0; i < len; i++) {
+                for (int i = 0; i < alen; i++) {
                     part = fmod(keys[k].wave_part + i, tone);
                     if (part <= htone) {
                         audio[i] = sin(part / sine_tone) * volume;
@@ -180,7 +324,7 @@ void AudioCallback(void *userdata, Uint8 *stream, int len){
                 double qtone = tone * 0.35;
                 double htone = tone * 0.5;
                 double ttone = tone * 0.75;
-                for (int i = 0; i < len; i++) {
+                for (int i = 0; i < alen; i++) {
                     part = fmod(keys[k].wave_part + i, tone);
                     if (part <= qtone) {
                         audio[i] = sin(part / sine_tone) * volume;
@@ -199,6 +343,22 @@ void AudioCallback(void *userdata, Uint8 *stream, int len){
             keys[k].wave_part = part;
         }
     }
+    return pressed;
+}
+
+void AudioCallback(void *userdata, Uint8 *stream, int len){
+    Keys *keys = (Keys*)userdata;
+
+    // first ensure silence in the stream
+    SDL_memset(stream, have.silence, len);
+
+    // create an audio stream to contain all pressed keys in one
+    Sint32 *audio = SDL_malloc(sizeof(Sint32) * len);
+    SDL_memset(audio, 0, sizeof(Sint32) * len);
+
+    int pressed = 0;
+    pressed += addFrequencies(audio, len, keys->white, keys->w_len);
+    pressed += addFrequencies(audio, len, keys->black, keys->b_len);
 
     // normalize our audio into the stream
     if (pressed) {
@@ -217,9 +377,15 @@ void AudioCallback(void *userdata, Uint8 *stream, int len){
 
 int main() {
     extern WaveForm wave;
-    // create our keys data structure
-    Key keys[256];
-    setupKeys(keys);
+    // create our keys data structures
+    Key white[36];
+    Key black[25];
+    Keys keys;
+    keys.white = white;
+    keys.w_len = 36;
+    keys.black = black;
+    keys.b_len = 25;
+    setupKeys(&keys, 'C', 2, 'C', 7);
 
     // we cannot make this a console only application because there are 
     // no keyboard events unless we have video initialized
@@ -233,28 +399,39 @@ int main() {
     SDL_Window *window;
     SDL_Renderer *renderer;
         
-    if(SDL_CreateWindowAndRenderer(1300, 220, 0, &window, &renderer)) {
+    if(SDL_CreateWindowAndRenderer(1296, 220, 0, &window, &renderer)) {
         printf("Could not create window and renderer: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Surface *image = IMG_Load("piano.png");
-    if (image == NULL){
-        printf("Failed to load image\n");
-        printf("SDL Says: %s\n", SDL_GetError());
-        return 1;
+    SDL_Rect white_keys[36];
+    for (int i =0; i<36; i++) {
+        white_keys[i].x = i * 36;
+        white_keys[i].y = 0;
+        white_keys[i].w = 36;
+        white_keys[i].h = 220;
     }
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
-    if (texture == NULL){
-        printf("Failed to create texture: %s\n", SDL_GetError());
-        return 1;
-    }
-    SDL_FreeSurface(image);
+    SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+    SDL_RenderFillRects(renderer, white_keys, 36);
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xaa);
+    SDL_RenderDrawRects(renderer, white_keys, 36);
 
-    // show image
-    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_Rect black_keys[25];
+    int dist = 24;
+    for (int i = 0; i < 25; i++) {
+        black_keys[i].x = dist + i * 36;
+        black_keys[i].y = 0;
+        black_keys[i].w = 24;
+        black_keys[i].h = 140;
+        if (i % 5 == 1 || i % 5 == 4) {
+            dist += 36;
+        }
+    }
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xaa);
+    SDL_RenderFillRects(renderer, black_keys, 25);
+    SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+    SDL_RenderDrawRects(renderer, black_keys, 25);
+
     SDL_RenderPresent(renderer);
 
 
@@ -268,7 +445,7 @@ int main() {
     want.channels = 1;
     want.samples = 1024;
     want.callback = AudioCallback;
-    want.userdata = keys;
+    want.userdata = &keys;
     dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FREQUENCY_CHANGE);
 
     if (dev == 0) {
@@ -327,9 +504,20 @@ int main() {
                 } else if (key >= 0 && key <= 127) {
                     mods = SDL_GetModState();
                     if (mods & KMOD_SHIFT) {
-                        key += 128;
+                        for (int i = 0; i < keys.b_len; i++) {
+                            if (keys.black[i].key == key) {
+                                keys.black[i].on = true;
+                                break;
+                            }
+                        }
+                    } else {
+                        for (int i = 0; i < keys.w_len; i++) {
+                            if (keys.white[i].key == key) {
+                                keys.white[i].on = true;
+                                break;
+                            }
+                        }
                     }
-                    keys[key].on = true;
                 }
                 break;
             case SDL_KEYUP:
@@ -337,9 +525,20 @@ int main() {
                 if (key >= 0 && key <= 127) {
                     mods = SDL_GetModState();
                     if (mods & KMOD_SHIFT) {
-                        key += 128;
+                        for (int i = 0; i < keys.b_len; i++) {
+                            if (keys.black[i].key == key) {
+                                keys.black[i].on = false;
+                                break;
+                            }
+                        }
+                    } else {
+                        for (int i = 0; i < keys.w_len; i++) {
+                            if (keys.white[i].key == key) {
+                                keys.white[i].on = false;
+                                break;
+                            }
+                        }
                     }
-                    keys[key].on = false;
                 }
                 break;
             default:
@@ -349,7 +548,6 @@ int main() {
 
 done: // cleanup
     SDL_CloseAudioDevice(dev);
-    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
